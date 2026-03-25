a78e1d69d7b8a62b6c77970a2085f68a33c8eb78
eduardogtz1678-crypto <eduardogtz1678@gmail.com>
2026-03-25 08:59:55 -0300

    Update visual.lua

--- visual.lua
+++ visual.lua
@@ -1,1 +1,14 @@
-
+spawn(function()
+    while task.wait(2) do
+        if _G.Settings.ESP then
+            for _,v in pairs(workspace:GetChildren()) do
+                if v:FindFirstChild("Humanoid") then
+                    if not v:FindFirstChild("HL") then
+                        local h = Instance.new("Highlight",v)
+                        h.Name="HL"
+                    end
+                end
+            end
+        end
+    end
+end)

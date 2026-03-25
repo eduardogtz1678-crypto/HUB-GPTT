ef84ded12adee6bc71c746d5e060f2c4ce72fb3f
eduardogtz1678-crypto <eduardogtz1678@gmail.com>
2026-03-25 08:58:21 -0300

    Update fruit.lua

--- fruit.lua
+++ fruit.lua
@@ -1,1 +1,16 @@
+local lastFruit
 
+workspace.DescendantAdded:Connect(function(v)
+    if v:IsA("Tool") and v.Name:lower():find("fruit") then
+        lastFruit = v
+    end
+end)
+
+spawn(function()
+    while task.wait() do
+        if _G.Settings.FruitTP and lastFruit and lastFruit:FindFirstChild("Handle") then
+            local root = game.Players.LocalPlayer.Character.HumanoidRootPart
+            root.CFrame = lastFruit.Handle.CFrame
+        end
+    end
+end)

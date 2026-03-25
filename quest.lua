5da4a079c22bc7c6a6dde8ee3e2e1e56041fe35b
eduardogtz1678-crypto <eduardogtz1678@gmail.com>
2026-03-25 08:59:26 -0300

    Update quest.lua

--- quest.lua
+++ quest.lua
@@ -1,1 +1,11 @@
-
+spawn(function()
+    while task.wait(2) do
+        if _G.Settings.AutoQuest then
+            for _,v in pairs(workspace:GetDescendants()) do
+                if v:IsA("ProximityPrompt") then
+                    fireproximityprompt(v)
+                end
+            end
+        end
+    end
+end)
